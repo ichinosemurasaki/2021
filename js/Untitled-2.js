c4361bddb27c7768f0a8e6f8630023439afa170e
ichinosemurasaki <ichinosemurasaki@gmail.com>
2021-12-13 16:33:07 +0800

    1213 edit

---
+++ js/Untitled-2.js
@@ -0,0 +1,20 @@
+$(function () {
+    setTimeout(function () {
+        $("#fd-in").css("opacity", "1"), 500
+    })
+    setTimeout(function () {
+        $(".circle-wh").css("transform", "scale(15)"), 500
+    })
+    setTimeout(function () {
+        $(".next").css("opacity", "1"), 500
+    })
+    $("#next").on("click", () => {
+        $(".circle-2").css({
+            "height": "10px",
+            "width": "10px",
+            "transform": "scale(250)"
+        }),
+            $("body").css("overflow", "hidden")
+        setTimeout(function () { window.location.href = "./Untitled-3.html"; }, 600);
+    })
+})
